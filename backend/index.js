7c43d9cd2cddea37efe44a2851887cbeb82b94aa
digitalAliSaif <digital.alisaif@gmail.com>
2025-03-10 11:33:33 +0500

    Backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("Server");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send(`<h2>hello from root</h2>`);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running on port:${port}`);
+});
