83fc8f6c5388063e403d44109551723c6ad1d655
digitalAliSaif <digital.alisaif@gmail.com>
2025-03-11 15:58:57 +0500

    Routers edit

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 
 app.use('/digitek-solutions', routerBranching);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server running on port:${port}`);
 });
