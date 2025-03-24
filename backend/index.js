3d6a846d6439159d24de2b2861a57801a3c62bc6
digitalAliSaif <digital.alisaif@gmail.com>
2025-03-24 13:35:52 +0500

    cors update

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import connectDB from "./db.js";
 import routerBranching from './routers/routeBranching.js';
 
@@ -7,6 +8,7 @@ const port = 3000;
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/digital-accountant', routerBranching);
